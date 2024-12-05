14e183bc2bb8ada96dca18f5678a93df75bd8ce5
Cassandra Gomez <cassandrakgomez@gmail.com>
2024-12-05 01:53:45 -0600

    Anakin (Enemy) Moves Back and Forth

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,46 @@ public class Enemy : MonoBehaviour
 {
     [Header("Enemy Settings")]
     [SerializeField] private int damage = 1; // Amount of damage this enemy does
+    [SerializeField] private int speed = 5; // Speed of the enemy
+    [SerializeField] private int range = 5; // Range of the enemy
+
+    private Vector2 startPos;
+    private Vector2 targetPos;
+    private Vector2 subPos;
+    private bool pos_check;
+
+
+     void Start(){
+        startPos = transform.position;
+        targetPos = new Vector2(startPos.x - range, startPos.y);
+        pos_check = true;
+    }
+
+    void FixedUpdate(){
+        EvilMovement();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Creature>().TakeDamage(damage);
+        }
+    }
+
+    public void EvilMovement(){
+        Vector2 currentLoc = transform.position;
+        transform.position = Vector2.MoveTowards(currentLoc, targetPos, speed * Time.deltaTime);
+        if (Vector2.Distance(currentLoc, targetPos) < 0.1f){
+            
+            if(pos_check){
+                targetPos = new Vector2(startPos.x + range, startPos.y);
+            }
+            else{
+                targetPos = new Vector2(startPos.x - range, startPos.y);
+            }
+
+            pos_check = !pos_check;
         }
     }
 }
